7d483c315d3df89a5f0cf0d01dbac395d47c79ce
Maryonete <mauricemarion@hotmail.com>
2024-11-13 12:03:54 +0100

    add db_file configuration DB

--- src/Entity/Database.php
+++ src/Entity/Database.php
@@ -6,7 +6,7 @@ use PDO;
 
 class Database
 {
-    // const DB_FILE = "../../config/db_file";
+    const DB_FILE = "../config/db_file.php";
 
     /**
      * @var PDO|null Instance unique de PDO
@@ -29,15 +29,11 @@ class Database
      */
     public static function getPDO(): PDO
     {
+
         if (self::$instance === null) {
-
-
-            $dsn = sprintf(
-                'mysql:dbname=%s;host=%s',
-                'job',
-                'localhost'
-            );
-            self::$instance = new \PDO($dsn, 'root', '', [
+            $config = self::parseConfigFile(self::DB_FILE);
+            $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};charset=utf8";
+            self::$instance = new PDO($dsn, $config['user'], $config['password'], [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
             ]);
@@ -47,5 +43,28 @@ class Database
     public static function setPDO(PDO $pdo): void
     {
         self::$instance = $pdo;
+    }
+
+
+    private static function  parseConfigFile($filename)
+    {
+        $config = [];
+        $lines = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+        foreach ($lines as $line) {
+            // Supprimer les espaces en début et fin de ligne
+            $line = trim($line);
+
+            // Ignorer les lignes de commentaire
+            if (strpos($line, ';') === 0) continue;
+
+            // Découper la ligne en clé/valeur
+            list($key, $value) = explode('=', $line, 2);
+
+            // Nettoyer les retours à la ligne
+            $config[trim($key)] = trim($value);
+        }
+
+        return $config;
     }
 }
